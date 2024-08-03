094df83ebfe50855d597525e4fe1a91ef38682da
pazi <35765732+zhangjiezhang@users.noreply.github.com>
2024-08-03 19:44:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,21 +3,60 @@ package main
 import (
 	"bytes"
 	"flag"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"net/http"
 	"os/exec"
 	"regexp"
+	"strconv"
 )
 
+var (
+	miPlugPower = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "mi_plug_power",
+			Help: "小米智能插座功耗",
+		},
+		[]string{"name"},
+	)
+	miPlugTemperature = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "mi_plug_temperature",
+			Help: "小米智能插座温度",
+		},
+		[]string{"name"},
+	)
+	name  = ""
+	ip    = ""
+	token = ""
+)
+
+func init() {
+	prometheus.MustRegister(miPlugPower)
+	prometheus.MustRegister(miPlugTemperature)
+}
+
 func main() {
-	var ip, token string
+	flag.StringVar(&name, "name", "", "drive name")
 	flag.StringVar(&ip, "ip", "", "drive ip")
 	flag.StringVar(&token, "token", "", "drive token")
 	flag.Parse()
-	if len(ip) == 0 || len(token) == 0 {
+	if len(name) == 0 || len(ip) == 0 || len(token) == 0 {
 		log.Printf("param is null")
 		return
 	}
+	go func() {
+		for {
+			callMiioctl()
+		}
+	}()
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
+}
 
+func callMiioctl() {
 	cmd := exec.Command("miiocli", "genericmiot", "--ip", ip, "--token", token, "status")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
@@ -28,16 +67,32 @@ func main() {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 		return
 	}
-	log.Printf("%s\n", outStr)
+	// log.Printf("%s\n", outStr)
 	if len(errStr) != 0 {
-		log.Printf("errorStr")
-		log.Printf("%s\n", errStr)
+		// log.Printf("errorStr")
+		log.Printf("%s", errStr)
 	}
 	if len(outStr) == 0 {
 		return
 	}
 	power, temperature := parseData(outStr)
 	log.Printf("功耗：%s W, 温度：%s C", power, temperature)
+	if len(power) != 0 {
+		powerFloat, err := strconv.ParseFloat(power, 64)
+		if err != nil {
+			log.Printf("Error: %s", err)
+		} else {
+			miPlugPower.WithLabelValues("name", name).Set(powerFloat)
+		}
+	}
+	if len(temperature) != 0 {
+		temperatureFloat, err := strconv.ParseFloat(temperature, 64)
+		if err != nil {
+			log.Printf("Error: %s", err)
+		} else {
+			miPlugTemperature.WithLabelValues("name", name).Set(temperatureFloat)
+		}
+	}
 }
 
 // power-consumption:electric-power
